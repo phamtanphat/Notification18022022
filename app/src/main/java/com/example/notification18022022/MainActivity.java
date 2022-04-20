02524ee634b4cc0edf7c38143f6807b86abc24fc
Phat <phatdroid94@gmail.com>
2022-04-20 19:09:29 +0700

    event click notification

--- app/src/main/java/com/example/notification18022022/MainActivity.java
+++ app/src/main/java/com/example/notification18022022/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.notification18022022;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,20 +27,27 @@ public class MainActivity extends AppCompatActivity {
         mBtnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID);
                 builder.setContentTitle("Thông báo");
                 builder.setContentText("Bạn có 5 voucher sắp hết hạn. Hãy vào app kiểm tra!!");
                 builder.setShowWhen(true);
                 builder.setSmallIcon(android.R.drawable.btn_star);
+                builder.setVibrate(new long[]{1000, 200, 2000, 200});
+                builder.setContentIntent(pendingIntent);
 
                 NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,"abc", NotificationManager.IMPORTANCE_DEFAULT);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Admob", NotificationManager.IMPORTANCE_DEFAULT);
                     notificationManager.createNotificationChannel(notificationChannel);
                 }
 
-                notificationManager.notify(1,builder.build());
+                notificationManager.notify(1, builder.build());
 
             }
         });
